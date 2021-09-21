c2693066edccaaf260bcabd201ad7e904045f7cf
Robby Suliantoro <robby.suliantoro@gmail.com>
2021-09-21 22:17:16 +0700

    ig

---
+++ app/src/main/java/com/robby/instagramfeedclonechapter4tugas/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.robby.instagramfeedclonechapter4tugas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
